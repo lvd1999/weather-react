17a53a15eba53b42885a541650ee9cbefff34d11
Patrick <patricklee0221@gmail.com>
2021-10-18 14:43:40 +0100

    api and search bar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const api = {
+  key: 'b88c97ba7ed736121b9d4c9442253be8',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="search..." />
+        </div>
+      </main>
     </div>
   );
 }
